f247d5cfd148cb053002ddbecd6300de431e090f
ofhatwa <tofhatwa@gmail.com>
2024-10-09 19:02:58 +0200

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
+  var newTask;
+  var newTask2;
 
   if (trueOrFalse) {
-    var newTask = true;
-    var newTask2 = false;
+    newTask = true;
+    newTask2 = false;
   }
 
   return [task, task2];

2a6284c1bbe24629db997bdaea45bc5266a975db
ofhatwa <tofhatwa@gmail.com>
2024-10-09 21:34:16 +0200

    Adding task 1, The teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+// Define the Student interface with the required properties
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience: number; 
+    location: string;
+    [key: string]: any; 
+}
+

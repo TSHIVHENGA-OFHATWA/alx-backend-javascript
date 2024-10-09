934c84db0099d6fdf77f64f3cab4dae44b422932
ofhatwa <tofhatwa@gmail.com>
2024-10-09 23:07:42 +0200

    Updated main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,5 +1,5 @@
 // Define the Student interface with the required properties
-interface Teacher {
+export interface Teacher {
     firstName: string;
     lastName: string;
     fullTimeEmployee: boolean;
@@ -8,3 +8,56 @@ interface Teacher {
     [key: string]: any; 
 }
 
+// Interface for the function
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Define an interface for the constructor
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Define an interface for the StudentClass
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
